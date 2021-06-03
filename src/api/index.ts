ac4a19c8f067a10f7d5f5b37f377b15300ebf635
Micah Akpan <makpan@gidimobile.com>
2021-06-03 13:35:41 -0400

    Config env vars for dynamic deployment

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const hostedAPIURL = 'https://questionerv1.herokuapp.com/api/v1';
-const localHostURL = `http://localhost:${process.env.PORT}/api/v1`;
+const localHostURL = `http://localhost:${process.env.DEV_API_PORT}/api/v1`;
 const url = process.env.NODE_ENV === 'development' ? localHostURL : hostedAPIURL;
 
 const axiosInstance = axios.create({
